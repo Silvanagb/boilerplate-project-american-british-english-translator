6c316f6615bd2d92422fb6f9edf61c283796bea6
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-07-04 14:29:54 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,25 @@
-'use strict';
+app.route('/api/translate')
+  .post((req, res) => {
+    const { text, locale } = req.body;
 
-const Translator = require('../components/translator.js');
-const translator = new Translator();
+    if (text === undefined || locale === undefined) {
+      return res.json({ error: 'Required field(s) missing' });
+    }
 
-module.exports = function (app) {
-  app.route('/api/translate')
-    .post((req, res) => {
-      const { text, locale } = req.body;
+    if (text === '') {
+      return res.json({ error: 'No text to translate' });
+    }
 
-      if (text === undefined || locale === undefined) {
-        return res.json({ error: 'Required field(s) missing' });
-      }
+    if (locale !== 'american-to-british' && locale !== 'british-to-american') {
+      return res.json({ error: 'Invalid value for locale field' });
+    }
 
-      if (text === '') {
-        return res.json({ error: 'No text to translate' });
-      }
+    const rawTranslation = translator.translate(text, locale);
 
-      if (locale !== 'american-to-british' && locale !== 'british-to-american') {
-        return res.json({ error: 'Invalid value for locale field' });
-      }
+    if (rawTranslation === text) {
+      return res.json({ text, translation: 'Everything looks good to me!' });
+    }
 
-      const translation = translator.translateAndHighlight(text, locale);
-
-      if (translation === text) {
-        return res.json({ text, translation: 'Everything looks good to me!' });
-      }
-
-      return res.json({ text, translation });
-    });
-};
+    const highlighted = translator.translateAndHighlight(text, locale);
+    return res.json({ text, translation: highlighted });
+  });
