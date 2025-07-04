224352c7614a817e11e542e685c6de31ead0441f
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-07-04 13:58:02 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,37 +3,34 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
+  
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      const { text, locale } = req.body;
-
-      if (text === undefined || locale === undefined) {
-  return res.status(200).json({ error: 'Required field(s) missing' });
-}
-
-if (text.trim() === '') {
-  return res.status(200).json({ error: 'No text to translate' });
-}
-
-      if (!['american-to-british', 'british-to-american'].includes(locale)) {
-        return res.status(200).json({ error: 'Invalid value for locale field' });
+      //console.log(req.body.text);
+      //console.log(req.body.locale);
+      if(req.body.text == undefined || req.body.locale == undefined) {
+        return res.status(200).json({error: 'Required field(s) missing'});
       }
-
-      const plain = translator.translate(text, locale);
-      const highlighted = translator.translateAndHighlight(text, locale);
-
-      if (plain === text) {
+      if(req.body.text == '') {
+        return res.status(200).json({error: 'No text to translate'});
+      }
+      if(req.body.locale != 'american-to-british' && req.body.locale != 'british-to-american') {
+        return res.status(200).json({error: 'Invalid value for locale field'});
+      }
+      let strTranslated = translator.translateAndHighlight(req.body.text, req.body.locale);
+      if(strTranslated === req.body.text) {
         return res.status(200).json({
-          text,
-          translation: "Everything looks good to me!"
+          text: req.body.text,
+          translation: 'Everything looks good to me!'
         });
       }
-
       return res.status(200).json({
-        text,
-        translation: highlighted
+        text: req.body.text,
+        translation: strTranslated
       });
     });
+  //
+  
 };
